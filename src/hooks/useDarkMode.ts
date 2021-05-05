fd8a7cd9768b2fad20bceba94b060f70de8d41f9
Pete Beegle <pete.beegle@gmail.com>
2021-05-04 22:34:52 -0400

    fix linting errors across project

--- src/hooks/useDarkMode.ts
+++ src/hooks/useDarkMode.ts
@@ -1,15 +1,15 @@
 import { Dispatch, SetStateAction, useEffect, useState } from 'react';
 
-export default function useDarkMode(): [string, Dispatch<SetStateAction<String>>] {
+export default function useDarkMode(): [
+  string,
+  Dispatch<SetStateAction<string>>
+] {
   const [theme, setTheme] = useState('light');
   const colorTheme = theme === 'light' ? 'dark' : 'light';
-  useEffect(
-    () => {
-      const root = window.document.documentElement;
-      root.classList.remove(colorTheme);
-      root.classList.add(theme);
-    },
-    [theme]
-  );
+  useEffect(() => {
+    const root = window.document.documentElement;
+    root.classList.remove(colorTheme);
+    root.classList.add(theme);
+  }, [theme]);
   return [colorTheme, setTheme];
 }
