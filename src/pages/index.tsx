51f1d3fc94f9f0567cabaac6313e0223bbdb729d
Pete Beegle <pete.beegle@gmail.com>
2021-08-17 20:00:39 -0400

    add text color styling to external link component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,7 +7,7 @@ const HomePage = (): JSX.Element => (
       <h1 className="text-gray-900 dark:text-white font-semibold text-6xl tracking-wide leading-loose">
         pete beegle
       </h1>
-      <div className="text-gray-600 dark:text-gray-300 flex items-center justify-center">
+      <div className="flex items-center justify-center">
         <ExternalLink
           className="px-1"
           href="https://github.com/petebeegle"
