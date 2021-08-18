3193a0b83a7dc6fc80e66fcd266dd410fc4ab45a
Pete Beegle <pete.beegle@gmail.com>
2021-08-17 20:01:32 -0400

    add footer component

--- src/components/layouts/DefaultLayout.tsx
+++ src/components/layouts/DefaultLayout.tsx
@@ -1,4 +1,5 @@
 import Header from '@module/Header/Header';
+import Footer from '@module/Footer/Footer';
 
 type Props = {
   children: React.ReactNode;
@@ -8,6 +9,7 @@ const DefaultLayout = ({ children }: Props): JSX.Element => (
   <div className="flex flex-col h-screen">
     <Header />
     <div className="flex-grow">{children}</div>
+    <Footer />
   </div>
 );
 
